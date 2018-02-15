08537560d92e393604996784752b94b1e434fe25
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2018-02-15 19:23:58 +0200

    Hello electron

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,26 @@
-const electron = require('electron')
 
+import * as electron from 'electron'
+
+import { app, BrowserWindow } from 'electron'
+
+function initMain(): Promise<Electron.BrowserWindow> {
+  return new Promise((resolve, reject) => {
+    app.on('window-all-closed', function () {
+      if (process.platform != 'darwin') {
+        app.quit();
+      }
+    });
+
+    app.on('ready', function () {
+      const main = new BrowserWindow({ width: 800, height: 600 })
+      main.loadURL('http://localhost:1234')
+      resolve(main)
+    })
+  })
+}
+
+const mainWindow = initMain()
+
+mainWindow.then((window: Electron.BrowserWindow) => {
+
+})
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App'
+import App from 'renderer/App'
 
 const rootDiv = document.getElementById('root') as HTMLElement
 
