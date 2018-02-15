41cb7298d3e00a5bce33d3401afcbab9ea8bb4ca
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2018-02-15 19:05:05 +0200

    Bundle renderer with parcel

---
+++ src/renderer/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
+
+const rootDiv = document.getElementById('root') as HTMLElement
+
+ReactDOM.render(<App />, rootDiv)
